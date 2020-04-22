15bb5d6d8e61ecfdb31ea35a9c5b3fd4b19f0148
scolas <scolas481@gmail.com>
2020-04-22 19:35:25 -0400

    axios removed

--- src/components/store/modules/todos.js
+++ src/components/store/modules/todos.js
@@ -1,5 +1,5 @@
 //state getters
-import axios from 'axios';
+//import axios from 'axios';
 
 const state = {
     todos: [
@@ -20,29 +20,7 @@ const getters = {
 };
 
 const actions = {
-    async fetchTodos({ commit }){
-        const response = await axios.get(
-            'https://jsonplaceholder.typicode.com/todos'
-        );
-       // console.log(response+""+response.data);
-       commit('setTodos', response.data);
-    },
-    async addTodo({ commit }, title){
-        //completed is the status
-        console.log("add to b4 post");
-        const response = await axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false });
-        console.log("todo added"+response.data);
-        //commit a mutation/ response data has the new todo nad that will updte the list
-        commit('newTodo', response.data);
-    },
-    async deleteTodo({ commit }, id){
-        //this removes it from back end
-        //commit removes it from User interface
-        // in commit last part says filter and for each todo return where the todo id is not equal  to the id thats passed in 
-        await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
-        console.log("todo deleted");
-        commit('removeTodo', id)
-    }
+
 };
 
 //mutates the state
